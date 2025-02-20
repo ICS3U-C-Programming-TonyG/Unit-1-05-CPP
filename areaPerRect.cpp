6d9364595cd36327d9abd798dce85a13b5b4c252
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-02-20 16:35:25 +0000

    understands the dimensions of a rectangle

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// copyright (c) 2025 Tony G All rights reserved
+//
+// created by Tony G
+// created on 2025-02-19
+// this program tells you the dimensions of a 10x10 rectangle
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "If the dimensions of a rectangle is 10cm x 10cm" << std::endl;
+    std::cout << "The area is = " << (10*10)<< std::endl;
+    std::cout << "The perimeter is = " << (2*10+2*10)<< std::endl;
+}
